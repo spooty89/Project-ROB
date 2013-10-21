2cae8c19c89358c475a757dc944b98a8914b20c4
yemount@gmail.com <shanhuan@andrew.cmu.edu>
2013-10-21 12:45:21 -0400

    Added transition box

---
+++ Assets/TransitionAnimSequence.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransitionAnimSequence : MonoBehaviour {
+	public float Hello = 0.5f;
+	public string[] directions = {""};
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
